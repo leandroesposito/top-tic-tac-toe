0b89ffc6f437bb2c5170743dbc861b56b1870ace
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-02-14 18:09:25 -0300

    Create main logic structure

---
+++ script.js
@@ -0,0 +1,20 @@
+function Player(name, color) {
+
+}
+
+function Gameboard() {
+
+}
+
+function Game(player1, player2) {
+
+    const getStatus = () => {
+
+    }
+
+    return { getStatus };
+}
+
+const ScreenController = (function () {
+
+})();
