52fb796f96dbc0bea388c7aa681433c3f6862049
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-02-14 19:50:58 -0300

    Create Player and Gameboard classes

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
 function Player(name, color) {
+    function getName() {
+        return name;
+    }
 
+    function getColor() {
+        return color;
+    }
+
+    return {
+        getName,
+        getColor
+    };
 }
 
 function Gameboard() {
+    const board = [];
 
+    for (let i = 0; i < 3; i++) {
+        board.push([null, null, null]);
+    }
+
+    function getBoard() {
+        return board.map(row => [...row]);
+    }
+
+    function placeMarker(player, row, column) {
+        if (board[row][column] === null) {
+            board[row][column] = player;
+            return true;
+        }
+        return false;
+    }
+
+    return {
+        getBoard,
+        placeMarker
+    };
 }
 
 function Game(player1, player2) {
