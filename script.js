1d2b79e1e81a4d7276574618339103ce053c94be
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-02-14 21:03:56 -0300

    Create Game class

--- script.js
+++ script.js
@@ -39,12 +39,83 @@ function Gameboard() {
 }
 
 function Game(player1, player2) {
+    const board = board();
+    const currentPlayer = player1;
 
-    const getStatus = () => {
-
+    function changePlayerTurn() {
+        if (currentPlayer === player1) {
+            currentPlayer = player2;
+        }
+        else {
+            currentPlayer = player1;
+        }
     }
 
-    return { getStatus };
+    function playRound(row, column) {
+        if (currentPlayer !== null) {
+            if (board.placeMarker(currentPlayer, row, column)) {
+                changePlayerTurn();
+            }
+        }
+    }
+
+    function getWinner() {
+        for (let i = 0; i < 3; i++) {
+            // row i
+            if (board[i][0] !== null && board[i][0] == board[i][1] && board[i][0] == board[i][2]) {
+                return board[i][0];
+            }
+            // column i
+            if (board[0][i] !== null && board[0][i] == board[1][i] && board[0][i] == board[2][i]) {
+                return board[0][i];
+            }
+        }
+
+        if (board[1][1] !== null) {
+            if (
+                // negative diagonal
+                (board[0][0] === board[1][1] && board[0][0] === board[2][2]) ||
+                // positive diagonal
+                (board[2][0] === board[1][1] && board[2][0] === board[0][2])
+            ) {
+                return board[1][1];
+            }
+        }
+
+        return null;
+    }
+
+    function isDraw() {
+        for (let row = 0; row < 3; row++) {
+            for (let column = 0; column < 3; column++) {
+                if (board[row][column] === null) {
+                    false;
+                }
+            }
+        }
+        return true;
+    }
+
+    function getStatus() {
+        const winner = getWinner();
+
+        if (winner) {
+            currentPlayer = null;
+            return `${winner.getName()} won the game.`;
+        }
+
+        if (isDraw()) {
+            return "Draw.";
+        }
+
+        return `Is ${currentPlayer.getName}'s turn.`
+    }
+
+    return {
+        getStatus,
+        getBoard: board.getBoard(),
+        playRound
+    };
 }
 
 const ScreenController = (function () {
